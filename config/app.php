d7b85bf1ee44a6da7b340ba291459ec83053f361
ambagasdowa <ambagasdowa@gmail.com>
2018-03-01 13:54:54 -0600

    remove plugin paper for add as submodule

--- config/app.php
+++ config/app.php
@@ -221,7 +221,7 @@ return [
       'default' => [
           'className' => 'Cake\Database\Connection',
           'driver' => 'Cake\Database\Driver\Mysql',
-          'persistent' => false,
+          'persistent' => true,
           'host' => 'localhost',
           /**
            * CakePHP will use the default DB port based on the driver selected
